dee20b56731f60cb8f6976a3a6ddf2ac55e84d16
hank <ga895635@gmail.com>
2023-04-06 10:48:45 +0800

    AI圖片生成

--- client/src/components/customers.js
+++ client/src/components/customers.js
@@ -4,18 +4,29 @@ const Customers=()=>{
 
   const [question,setQuestion] =useState("")
   const [aiAns,setAiAns] =useState("")
-
+  const [aiImg, setAiImg] =useState("")
+  const [imgOrAns ,setImgOrAns] = useState(0)
   const Submit=()=>{ 
     fetch(`/api/openai?question=${question}`)
       .then(res => {    
         return res.json()
       })
-      .then(customers => {
-        console.log(customers )
-        setAiAns(customers[0].choices[0].text )
-        
-        // setQuestion("")
+      .then(customers => {   
+        console.log(customers)
+        setAiAns(customers[0].choices[0].text )       
       });
+    setImgOrAns(1)
+  }
+
+  const AIimgSubmit=()=>{
+    fetch(`/api/openaiimg?question=${question}`).then(res => {    
+      return res.json()
+    })
+    .then(josn => {       
+      console.log("josn",josn)
+      setAiImg(josn)
+    });
+    setImgOrAns(2)
   }
  
     return (
@@ -23,10 +34,20 @@ const Customers=()=>{
         <h2>AI回答</h2>
         <input value={question} onChange={(e)=>setQuestion(e.target.value)}/>
         <button onClick={Submit}>送出</button>
+        <button onClick={AIimgSubmit}> 圖片生成</button>
+        {imgOrAns== 1 &&( 
         <div>
           <p>Q: {question}</p>
           <p>A: {aiAns}</p>
         </div>
+        )}
+        {imgOrAns== 2 &&( 
+          <div>
+            <p>AI圖片</p>
+            <img src={aiImg}/>
+          </div>
+        )}
+        
       </div>
     );
   
--- server.js
+++ server.js
@@ -2,35 +2,86 @@
 import express from "express"
 import cors from"cors"
 import { Configuration, OpenAIApi } from "openai";
-
+import fetch from "node-fetch";
 
 const app = express();
 
-app.get('/api/customers', cors(), (req, res) => {
-  const customers = [
-    {id: 1, firstName: 'John', lastName: 'Doe'},
-    {id: 2, firstName: 'Brad', lastName: 'Traversy'},
-    {id: 3, firstName: 'Mary', lastName: 'Swanson'},
-    {id: 4, firstName: 'Dong', lastName: 'Hang'},
-  ];
+// app.get('/api/customers', cors(), (req, res) => {
+//   const customers = [
+//     {id: 1, firstName: 'John', lastName: 'Doe'},
+//     {id: 2, firstName: 'Brad', lastName: 'Traversy'},
+//     {id: 3, firstName: 'Mary', lastName: 'Swanson'},
+//     {id: 4, firstName: 'Dong', lastName: 'Hang'},
+//   ];
 
-  res.json(customers);
-});
+//   res.json(customers);
+// });
 
-
+ app.get('/api/openaiimg', cors(), async(req, res) => {
+  const {question} = req.query;
+  const prompt = question;
+  const model = "image-alpha-001";
+  const apiKey = "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";
+  async function generateImage() {
+    const headers = {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${apiKey}`,
+    };
+  
+    const data = JSON.stringify({
+      'model': model,
+      'prompt': prompt,
+      'num_images': 1,
+      'size': '512x512',
+      'response_format': 'url',    
+  
+    });
+    let dataUrl
+    try {
+      let response = await fetch('https://api.openai.com/v1/images/generations', {
+        method: 'POST',
+        headers,
+        body: data
+      });
+      let json = await response.json();
+      dataUrl =json.data[0].url   
+      return dataUrl
+      //  fetch('https://api.openai.com/v1/images/generations', {
+      //   method: 'POST',
+      //   headers,
+      //   body: data
+      // })
+      // .then(response=>{      
+      //   return response.json()
+      // })
+      // .then(json => console.log(json.data[0].url));
+      // .data.data[0].url
+   
+    } catch (error) {
+      console.error("error...",error);
+      return ""
+     
+    }
+   
+  }
+  let getRest = await generateImage(); 
+  res.json(getRest);
+ });
 
 app.get('/api/openai', cors(), async(req, res) => {
   const { question } = req.query;
   const getAIdata = async()=>{
     const configuration = new Configuration({
         organization: "org-995E46dMYrJB1aGmeg8F3r1f",
-        apiKey:"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",
+        apiKey:"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",
     });
     const openai = new OpenAIApi(configuration);
     
     const prompt =`
       ${question}
     `
+   
+    
     const response = await openai.createCompletion({
       model: "text-davinci-003",
       prompt: prompt,
